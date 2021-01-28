b87e34c852149a8a7e62551f1f021a1f0567be29
Juan Goncalves <juan.goncalves498@gmail.com>
2021-01-28 11:24:07 -0400

    Grunt: Enable view binding

--- app/src/main/java/com/jcgds/zuper/MainActivity.kt
+++ app/src/main/java/com/jcgds/zuper/MainActivity.kt
@@ -2,10 +2,16 @@ package com.jcgds.zuper
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.jcgds.zuper.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }
