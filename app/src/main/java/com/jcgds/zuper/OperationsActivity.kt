4c10f81f421d0b20c9f4baccd1480dcb3580a160
Juan Goncalves <juan.goncalves498@gmail.com>
2021-01-28 12:49:06 -0400

    Rename the `MainActivity` class to `OperationsActivity`

--- app/src/main/java/com/jcgds/zuper/OperationsActivity.kt
+++ app/src/main/java/com/jcgds/zuper/OperationsActivity.kt
@@ -2,15 +2,15 @@ package com.jcgds.zuper
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.jcgds.zuper.databinding.ActivityMainBinding
+import com.jcgds.zuper.databinding.OperationsActivityBinding
 
-class MainActivity : AppCompatActivity() {
+class OperationsActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: OperationsActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = OperationsActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 
